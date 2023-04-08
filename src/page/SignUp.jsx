b5fd8a913a726a9e071001a77e29f8bfae9dacda
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-08 14:34:56 +0900

    formState로 input값 관리

--- src/page/SignUp.jsx
+++ src/page/SignUp.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignUp = () => {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const onEmailChange = (e) => {
+    setFormState({ ...formState, email: e.target.value });
+  };
+  const onPasswordChange = (e) => {
+    setFormState({ ...formState, password: e.target.value });
+  };
+  
   return (
     <div>
-      <input data-testid='email-input' />
-      <input data-testid='password-input' />
+      <input
+        type='email'
+        data-testid='email-input'
+        value={formState.email}
+        onChange={onEmailChange}
+      />
+      <input
+        type='password'
+        data-testid='password-input'
+        value={formState.password}
+        onChange={onPasswordChange}
+      />
       <button data-testid='signup-button'>회원가입</button>
     </div>
   );
