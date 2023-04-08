99cc626ba4e0d1ccbc2cfcf93352a7bb1d95bc50
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-08 13:37:46 +0900

    Route 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import GlobalStyle from './styles/globalStyles';
+import { Routes, Route } from 'react-router-dom';
+import SignUp from './page/SignUp';
+import SignIn from './page/SignIn';
+import ToDo from './page/ToDo';
 
 function App() {
   return (
-    <div>
-      <GlobalStyle />
-    </div>
+    <Routes>
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/signin' element={<SignIn />} />
+      <Route path='/todo' element={<ToDo />} />
+    </Routes>
   );
 }
 
