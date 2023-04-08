bf2d747ac56862486a01912384b7ced6c6f126ec
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-08 15:40:58 +0900

    formState 구조 변경 및 각 input값 정규식 적용

--- src/page/SignUp.jsx
+++ src/page/SignUp.jsx
@@ -1,26 +1,47 @@
 import React, { useState } from 'react';
+import { regex } from '../util/regex';
 
 const SignUp = () => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({
+    email: {
+      value: '',
+      isVailed: false,
+    },
+    password: {
+      value: '',
+      isVailed: false,
+    },
+  });
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  console.log('formState', formState);
+
   const onEmailChange = (e) => {
-    setFormState({ ...formState, email: e.target.value });
+    const { value } = e.target;
+    const isVailed = regex.email.test(value);
+    setFormState({ ...formState, email: { value, isVailed } });
   };
   const onPasswordChange = (e) => {
-    setFormState({ ...formState, password: e.target.value });
+    const { value } = e.target;
+    const isVailed = regex.password.test(value);
+    setFormState({
+      ...formState,
+      password: { value, isVailed },
+    });
   };
-  
+
   return (
     <div>
       <input
         type='email'
         data-testid='email-input'
-        value={formState.email}
+        value={formState.email.value}
         onChange={onEmailChange}
       />
       <input
         type='password'
         data-testid='password-input'
-        value={formState.password}
+        value={formState.password.value}
         onChange={onPasswordChange}
       />
       <button data-testid='signup-button'>회원가입</button>
