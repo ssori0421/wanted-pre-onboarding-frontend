2fe71aa7d803f716c6a30a6248bf9910003f09bf
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-09 15:00:17 +0900

    회원가입 페이지 UI구현 및 button에 disabled 속성 부여

--- src/page/SignUp.jsx
+++ src/page/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { regex } from '../util/regex';
+import styled from 'styled-components';
 
 const SignUp = () => {
   const [formState, setFormState] = useState({
@@ -13,6 +14,14 @@ const SignUp = () => {
     },
   });
   const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    if (formState.email.isVailed && formState.password.isVailed) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [formState]);
 
   const onEmailChange = (e) => {
     const { value } = e.target;
@@ -28,31 +37,75 @@ const SignUp = () => {
     });
   };
 
-  useEffect(() => {
-    if (formState.email.isVailed && formState.password.isVailed) {
-      setIsDisabled(false);
-    } else {
-      setIsDisabled(true);
-    }
-  }, [formState]);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('회원가입 완료');
+  };
 
   return (
-    <div>
-      <input
-        type='email'
-        data-testid='email-input'
-        value={formState.email.value}
-        onChange={onEmailChange}
-      />
-      <input
-        type='password'
-        data-testid='password-input'
-        value={formState.password.value}
-        onChange={onPasswordChange}
-      />
-      <button data-testid='signup-button'>회원가입</button>
-    </div>
+    <SignUpContainer>
+      <StTitle>회원가입</StTitle>
+      <StInputForm onSubmit={onSubmit}>
+        <p>이메일</p>
+        <StInput
+          placeholder='아이디를 입력해 주세요.'
+          type='email'
+          data-testid='email-input'
+          value={formState.email.value}
+          onChange={onEmailChange}
+        />
+        <StMessage>'@'를 포함해서 이메일을 작성해 주세요.</StMessage>
+        <p>비밀번호</p>
+        <StInput
+          placeholder='비밀번호를 입력해 주세요.'
+          type='password'
+          data-testid='password-input'
+          value={formState.password.value}
+          onChange={onPasswordChange}
+        />
+        <StMessage>비밀번호는 8자 이상 작성해 주세요.</StMessage>
+        <Button disabled={isDisabled} data-testid='signup-button'>
+          회원가입
+        </Button>
+      </StInputForm>
+    </SignUpContainer>
   );
 };
 
 export default SignUp;
+
+const SignUpContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 24px;
+`;
+const StTitle = styled.h2`
+  margin: 16px 0px 48px;
+`;
+const StInputForm = styled.form`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 380px;
+  gap: 10px;
+`;
+
+const StInput = styled.input`
+  padding: 8px 15px 9px;
+  border-bottom: 1px solid #000;
+`;
+
+const StMessage = styled.p`
+  color: #35c5f0;
+`;
+
+const Button = styled.button`
+  height: 50px;
+  color: #fff;
+  font-size: 18px;
+  font-weight: 600;
+  border-radius: 8px;
+  background-color: ${(props) => (props.disabled ? 'grey' : '#35c5f0')};
+`;
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -5,7 +5,7 @@ body, button, dd, dl, dt, fieldset, form, h1, h2, h3, h4, h5, h6, input, legend,
 body, button, input, select, table, textarea {font-size:12px;line-height:16px;color:#202020;font-family:-apple-system, BlinkMacSystemFont, "Malgun Gothic", "맑은 고딕", helvetica, "Apple SD Gothic Neo", sans-serif}
 h1, h2, h3, h4, h5, h6 {font-size:inherit;line-height:inherit}
 textarea {-webkit-backface-visibility:hidden;backface-visibility:hidden;background-color:transparent;border:0;word-break:keep-all;word-wrap:break-word}
-button, input {-webkit-border-radius:0;border-radius:0;border:0}
+button, input {-webkit-border-radius:0;border-radius:0;border:0;outline: none}
 button {background-color:transparent}
 fieldset, img {border:0}
 img {vertical-align:top}
@@ -15,6 +15,7 @@ a {color:inherit;text-decoration:none}
 a:hover {text-decoration:underline}
 iframe {overflow:hidden;margin:0;border:0;padding:0;vertical-align:top}
 mark {background-color:transparent}
-i {font-style:normal}`;
+i {font-style:normal}
+`;
 
 export default GlobalStyle;
