5e896408ea840d2580bc42855c0a2983a52c50f1
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-10 00:05:31 +0900

    PageLayout 컴포넌트 추가

---
+++ src/components/PageLayout.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+const PageLayout = ({ children, title }) => {
+  return (
+    <PageContainer>
+      <StTitle>{title}</StTitle>
+      {children}
+    </PageContainer>
+  );
+};
+export default PageLayout;
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 24px;
+`;
+
+const StTitle = styled.h2`
+  font-size: 1.5rem;
+  margin: 16px 0px 48px;
+`;
