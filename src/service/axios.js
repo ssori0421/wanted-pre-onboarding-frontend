480fc634cefd284d67f6e5c58de23863e82bba05
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-09 23:59:37 +0900

    authInstance 추가 - add request header Authorization

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { getAccessToken } from '../util/localstorage';
 const defaultOptions = {
   baseURL: 'https://www.pre-onboarding-selection-task.shop/',
   headers: {
@@ -7,4 +8,15 @@ const defaultOptions = {
 };
 const instance = axios.create(defaultOptions);
 
-export { instance };
+const authInstance = axios.create(defaultOptions);
+
+authInstance.interceptors.request.use((config) => {
+  const access_token = getAccessToken();
+  if (access_token) {
+    const token = JSON.parse(access_token);
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export { instance, authInstance };
