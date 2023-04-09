03658a2b6b780b513c878f94eba0686d1ab55b2a
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-10 00:02:54 +0900

    useRouter에 pathName 추가

--- src/hooks/useRouter.js
+++ src/hooks/useRouter.js
@@ -1,12 +1,16 @@
 import { useNavigate } from 'react-router-dom';
 
 /**
- * @description - routeTo로 path값을 받아 페이지 이동
- * @returns routeTo
+ * @description
+ * - routeTo로 path값을 받아 페이지 이동
+ * - pathName 현재 url path를 가져옴
+ * @returns routeTo, pathName
  */
 export const useRouter = () => {
   const router = useNavigate();
+
   return {
     routeTo: (path) => router(path),
+    pathName: window.location.pathname,
   };
 };
