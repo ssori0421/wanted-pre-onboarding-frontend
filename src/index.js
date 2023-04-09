59dee2434ea449576a74787ed82f8ee2535f2bcc
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-10 00:03:29 +0900

    reset css 적용

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './styles/globalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <GlobalStyle />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
