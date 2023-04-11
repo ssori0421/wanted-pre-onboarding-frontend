879febccd579fad9655060bafd8c6ea323b69c75
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-11 23:09:00 +0900

    react router v6 적용해서 path 설정

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import ToDo from './page/ToDo';
 function App() {
   return (
     <Routes>
-      <Route path='/signup' element={<SignUp />} />
-      <Route path='/signin' element={<SignIn />} />
-      <Route path='/todo' element={<ToDo />} />
+      <Route path='signup' element={<SignUp />} />
+      <Route path='signin' element={<SignIn />} />
+      <Route path='todo' element={<ToDo />} />
     </Routes>
   );
 }
