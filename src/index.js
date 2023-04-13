80949ba640da3ab4a7dccf20040df466c8f35bb0
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-14 02:22:30 +0900

    BrowserRouter 수정

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import GlobalStyle from './styles/globalStyles';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <GlobalStyle />
       <App />
     </BrowserRouter>
